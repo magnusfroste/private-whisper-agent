53ebd7b90b351cc21d744e250b007cef7c253d93
privai4ever <privai4ever@gmail.com>
2026-03-04 18:01:14 +0100

    Add fluent-ffmpeg type definitions and update .gitignore

---
+++ fluent-ffmpeg.d.ts
@@ -0,0 +1,22 @@
+declare module 'fluent-ffmpeg' {
+  interface FfmpegCommand {
+    audioBits(value: number): FfmpegCommand
+    audioChannels(value: number): FfmpegCommand
+    audioFrequency(value: number): FfmpegCommand
+    format(value: string): FfmpegCommand
+    save(path: string): FfmpegCommand
+    on(event: 'end', callback: () => void): FfmpegCommand
+    on(event: 'error', callback: (err: Error) => void): FfmpegCommand
+    on(event: 'progress', callback: (progress: any) => void): FfmpegCommand
+    pipe(dest: any, spawn?: any): FfmpegCommand
+  }
+
+  interface FfmpegStatic {
+    (input: string | Buffer): FfmpegCommand
+    setFfmpegPath(path: string): void
+  }
+
+  const ffmpeg: FfmpegStatic
+
+  export = ffmpeg
+}
